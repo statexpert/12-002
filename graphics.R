f0fd12202024ccba288983b469425d6f060d90e1
Artem A Klevtsov <unikum.pm@gmail.com>
2012-08-01 02:43:19 +0700

    Чистка кода График объёма выборок ~ мощности для зависимых выборок

--- graphics.R
+++ graphics.R
@@ -6,11 +6,10 @@ groups <- 3
 
 # Функция вычисляет мощность для заданного объёма выборки
 # n - общий объём выборки
-power.test.anova <- sample.size.anova <- function(groups, n, f, sig = 0.05) {
+power.test.anova <- function(groups, n, f, sig = 0.05) {
   fit <- pwr.anova.test(k = groups, n = size/groups, f = f, sig.level = sig, power = NULL)
   power <- fit$power
 }
-
 # Расчёт мощности для всех f и sig и внесение значений в столбцы матрицы tab
 tab.size <- NULL
 for (i in sig) {
@@ -30,7 +29,8 @@ par(mar=c(8, 4, 4, 2) + 0.1, xpd = TRUE)
 matplot(size, tab.size, type = "l", lty = 1, col = colors, xlab = "Размер выборки", ylab = "Мощность", xlim = range(size), ylim = c(0, 1),  xaxs = "i", yaxs = "r")
 abline(h = 0.8, lty = "longdash", xpd = FALSE)
 title(main = "График зависимости мощности\n от размера выборки, размера эффекта\n и уровня значимости")
-legend(0, -0.6, legend = c("α=0.05; f=0.1", "α=0.05; f=0.25", "α=0.05; f=0.4", "α=0.01; f=0.1", "α=0.01; f=0.25", "α=0.04; f=0.1"), col = colors, lwd = 1, lty = 1, bty = "n", xpd = TRUE, xjust=0, yjust=0.5, ncol = 2)
+legend(0, -0.6, legend = c("α=0.05; f=0.1", "α=0.05; f=0.25", "α=0.05; f=0.4", "α=0.01; f=0.1", "α=0.01; f=0.25", "α=0.01; f=0.4"), col = colors, lwd = 1, lty = 1, bty = "n", xpd = TRUE, xjust=0, yjust=0.5, ncol = 2)
+par(mar=c(5, 4, 4, 2) + 0.1, xpd = FALSE)
 
 tab.power <- NULL
 f <- seq(0, 1, length = 100)
@@ -50,3 +50,29 @@ abline(h = 0.8, lty = "longdash", xpd = FALSE)
 title(main = "График зависимости мощности\nот размера эффекта (n=60)")
 legend(0, -0.4, legend = c("α=0.05", "α=0.01"), col = colors, lwd = 1, lty = 1, bty = "n", xpd = TRUE, xjust=0, yjust=0.5, ncol = 2)
 par(mar=c(5, 4, 4, 2) + 0.1, xpd=FALSE)
+
+d <- c(0.2, 0.5, 0.8)
+power.test.paired <- function(n, d, sig = 0.05) {
+  fit <- pwr.t.test(n = n, d = d, sig.level = sig, power = NULL, type = "paired")
+  power <- fit$power
+}
+# Расчёт мощности для всех f и sig и внесение значений в столбцы матрицы tab
+tab.size.p <- NULL
+for (i in sig) {
+  for (j in d) {
+    b <- power.test.paired(d = j, sig = i, n = size)
+    tab.size.p <- cbind(tab.size.p, b)
+    rm(b)
+  }
+}
+colnames(tab.size.p) <- rep(d,2)
+rownames(tab.size.p) <- size
+
+# График зависимости мощности от размеры выборки, размеры эффекта и уровня значимости для зависимых выборок
+colors <- rainbow(length(d)*length(sig))
+par(mar=c(8, 4, 4, 2) + 0.1, xpd = TRUE)
+matplot(size, tab.size.p, type = "l", lty = 1, col = colors, xlab = "Размер выборки", ylab = "Мощность", xlim = range(size), ylim = c(0, 1),  xaxs = "i", yaxs = "r")
+abline(h = 0.8, lty = "longdash", xpd = FALSE)
+title(main = "График зависимости мощности\n от размера выборки, размера эффекта\n и уровня значимости")
+legend(0, -0.6, legend = c("α=0.05; d=0.2", "α=0.05; d=0.5", "α=0.05; d=0.8", "α=0.01; d=0.2", "α=0.01; d=0.5", "α=0.01; d=0.8"), col = colors, lwd = 1, lty = 1, bty = "n", xpd = TRUE, xjust=0, yjust=0.5, ncol = 2)
+par(mar=c(5, 4, 4, 2) + 0.1, xpd = FALSE)
