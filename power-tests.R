62e65ee3d7e3edd8800f6d2bb241f4edf6fe08dc
Artem A Klevtsov <unikum.pm@gmail.com>
2012-08-01 02:43:52 +0700

    Мелочи

--- power-tests.R
+++ power-tests.R
@@ -13,7 +13,6 @@ sample.size.anova <- function(groups, n = NULL, f, sig = 0.05, b = 0.8) {
   fit <- pwr.anova.test(k = groups, f = f, sig.level = sig, power = b)
   size <- round(fit$n)*groups
 }
-
 # Сводные данные по расчёту объёма выборки для ANOVA
 ssa <- matrix(mapply(FUN = sample.size.anova, f = rep(f, 2), sig = sig, groups = groups), ncol = 2)
 rownames(ssa) <- f
@@ -26,7 +25,6 @@ sample.size.manova <- function(subgroups, v = NULL, f2, sig = 0.05, b = 0.8) {
   fit <- pwr.f2.test(u = (subgroups - 1), f2 = f2, sig.level = sig, power = b)
   size <- round(fit$v) + 1
 }
-
 # Сводные данные по расчёту объёма выборки для MANOVA
 ssm <- matrix(mapply(FUN = sample.size.manova, f2 = rep(f2, ), sig = sig, subgroups = subgroups), ncol = 2)
 rownames(ssm) <- f2
@@ -38,7 +36,6 @@ power.test.anova <- sample.size.anova <- function(groups, n, f, sig = 0.05) {
   fit <- pwr.anova.test(k = groups, n = round(n/groups), f = f, sig.level = sig, power = NULL)
   power <- round(fit$power, digits = 2)
 }
-
 # Мозность для выборки из 60 человек (3 группы)
 ssaf <- matrix(mapply(FUN = sample.size.anova, f = rep(f, 2), sig = sig, groups = groups, n = 60), ncol = 2)
 rownames(ssaf) <- f
@@ -50,7 +47,7 @@ sample.size.paired <- function(n = NULL, d, sig = 0.05, b = 0.8) {
   fit <- pwr.t.test(n = NULL, d = d, sig.level = sig, power = b, type = "paired")
   size <- round(fit$n)
 }
-
+# Оюъём выборки для зависимых выборок
 ssp <- matrix(mapply(FUN = sample.size.paired, d = rep(d, 2), sig = sig), ncol = 2)
 rownames(ssp) <- d
 colnames(ssp) <- sig
