9b2cfd8e643d8b671414541f299974be2e2177c6
Artem A Klevtsov <unikum.pm@gmail.com>
2012-08-01 02:18:38 +0700

    График мощность~размер эффекта к для n=60

--- graphics.R
+++ graphics.R
@@ -12,28 +12,41 @@ power.test.anova <- sample.size.anova <- function(groups, n, f, sig = 0.05) {
 }
 
 # Расчёт мощности для всех f и sig и внесение значений в столбцы матрицы tab
-tab <- NULL
+tab.size <- NULL
 for (i in sig) {
   for (j in f) {
     b <- power.test.anova(f = j, sig = i, groups = groups, n = size)
-    tab <- cbind(tab, b)
+    tab.size <- cbind(tab.size, b)
     rm(b)
   }
 }
+colnames(tab.size) <- rep(f,2)
+rownames(tab.size) <- size
 
-colnames(tab) <- rep(f,2)
-rownames(tab) <- size
+# График зависимости мощности\n от размера выборки, размера эффекта\n и уровня значимости на основании tab и size
 colors <- rainbow(length(f)*length(sig))
-
-par(mar=c(5, 4, 4, 2) + 0.1, xpd = FALSE)
-par(mar=c(5, 4, 4, 8))
-#par(xpd=NA,oma=c(0,0,0,0))
-#par(mar=c(5, 4, 4, 8))
-
-matplot(size, tab, type = "l", lty = 1, col = colors, xlab = "Размер выборки", ylab = "Мощность", xlim = range(size), ylim = c(0, 1),  xaxs = "i", yaxs = "i")
-
+#par(mar=c(5, 4, 4, 2) + 0.1, xpd = FALSE)
+par(mar=c(8, 4, 4, 2) + 0.1, xpd = TRUE)
+matplot(size, tab.size, type = "l", lty = 1, col = colors, xlab = "Размер выборки", ylab = "Мощность", xlim = range(size), ylim = c(0, 1),  xaxs = "i", yaxs = "r")
 abline(h = 0.8, lty = "longdash", xpd = FALSE)
 title(main = "График зависимости мощности\n от размера выборки, размера эффекта\n и уровня значимости")
+legend(0, -0.6, legend = c("α=0.05; f=0.1", "α=0.05; f=0.25", "α=0.05; f=0.4", "α=0.01; f=0.1", "α=0.01; f=0.25", "α=0.04; f=0.1"), col = colors, lwd = 1, lty = 1, bty = "n", xpd = TRUE, xjust=0, yjust=0.5, ncol = 2)
 
-legend(par()$usr[2], mean(par()$usr[3:4]), inset = c(-0.8, 0), legend = c("α=0.05; f=0.1", "α=0.05; f=0.25", "α=0.05; f=0.4", "α=0.01; f=0.1", "α=0.01; f=0.25", "α=0.04; f=0.1"), col = colors, lwd = 1, lty = 1, bty = "n", xpd = TRUE, xjust=0, yjust=0.5)
+tab.power <- NULL
+f <- seq(0, 1, length = 100)
+size=60
+for (i in sig) {
+  b <- power.test.anova(f = f, sig = i, groups = groups, n = size)
+  tab.power <- cbind(tab.power, b)
+  rm(b)
+}
+colnames(tab.power) <- sig
+
+# График зависимости мозности от размера эффекта и уровня значимости для выборки в 60 человек
+colors <- rainbow(length(sig))
+par(mar=c(6, 4, 4, 2) + 0.1, xpd = TRUE)
+matplot(f, tab.power, type = "l", lty = 1, col = colors, xlab = "Размер эффекта", ylab = "Мощность")
+abline(h = 0.8, lty = "longdash", xpd = FALSE)
+title(main = "График зависимости мощности\nот размера эффекта (n=60)")
+legend(0, -0.4, legend = c("α=0.05", "α=0.01"), col = colors, lwd = 1, lty = 1, bty = "n", xpd = TRUE, xjust=0, yjust=0.5, ncol = 2)
 par(mar=c(5, 4, 4, 2) + 0.1, xpd=FALSE)
