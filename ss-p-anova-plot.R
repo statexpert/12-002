0748a55fe6a93998e338af0840c69c49f998db2a
Artem A Klevtsov <unikum.pm@gmail.com>
2012-08-04 14:04:34 +0700

    Легенды перемещены в функцию title Убран цикл для abline

--- ss-p-anova-plot.R
+++ ss-p-anova-plot.R
@@ -25,13 +25,13 @@ colors <- rainbow(length(f)*length(sig))
 # Задаём отсутпы
 par(mar=c(8, 4, 4, 2) + 0.1, xpd = TRUE)
 # Строим график по tab.size
-matplot(size, tab.size, type = "l", lwd = 2, lty = 1, col = colors, xlab = "Размер выборки", ylab = "Мощность", xlim = c(0, max(size)), ylim = c(0, 1), xaxs = "i", yaxs = "r", xaxt = "n", cex.axis = 0.8)
+matplot(size, tab.size, type = "l", lwd = 2, lty = 1, col = colors, xlim = c(0, max(size)), ylim = c(0, 1), xaxs = "i", yaxs = "r", xaxt = "n", cex.axis = 0.8, xlab = "", ylab = "")
 # Добавляем ось x
-axis(1, at= seq(0, 400, by = 25), cex.axis = 0.8)
+axis(1, at = seq(0, 400, by = 25), cex.axis = 0.8)
 # Добавляем пунктир на b=0.8
 abline(h = 0.8, lty = "longdash", lwd = 0.5, xpd = FALSE)
 # Добавляем заголовок
-title(main = "График зависимости мощности\n от размера выборки, размера эффекта\n и уровня значимости")
+title(main = "График зависимости мощности\n от размера выборки, размера эффекта\n и уровня значимости", xlab = "Размер выборки", ylab = "Мощность")
 # Добавляем легенду
 legend(0, -0.6, legend = c("p=0.05; f=0.1", "p=0.05; f=0.25", "p=0.05; f=0.4", "p=0.01; f=0.1", "p=0.01; f=0.25", "p=0.01; f=0.4"), col = colors, lwd = 1, lty = 1, bty = "n", xpd = TRUE, xjust=0, yjust=0.5, ncol = 2)
 
@@ -40,6 +40,6 @@ points <- sort(mapply(FUN = sample.size.anova, f = rep(f, 2), sig = sig, groups 
 # Добавляем точки
 for (i in points) points(i, 0.8, pch = 20)
 # Добавляем пунктирные линии
-for (i in points) abline(v = i, lty = "longdash", lwd = 0.5, xpd = FALSE)
+abline(v = points, lty = "longdash", lwd = 0.5, xpd = FALSE)
 #for (i in size.x) text(i + 20, 0.03, labels = i)
 par(opar)
